98b3916e0016695d2a9d6518990ab50803fce2c2
Nacva28 <giorginacvlishvili@gmail.com>
2024-02-03 18:57:07 +0400

    end-about-start-train-list

--- Desktop/test-railway-webapp/src/app/app-routing.module.ts
+++ Desktop/test-railway-webapp/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { CustomerInfoComponent } from './components/customer-info/customer-info.component';
+import { TrainListComponent } from './components/train-list/train-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'customer-info', component: CustomerInfoComponent },
+  {path:'train-list',component:TrainListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Desktop/test-railway-webapp/src/app/app.module.ts
+++ Desktop/test-railway-webapp/src/app/app.module.ts
@@ -4,10 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ComponentsComponent } from './components/components.component';
+import { TrainListComponent } from './components/train-list/train-list.component';
+import { CustomerInfoComponent } from './components/customer-info/customer-info.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    TrainListComponent,
+    CustomerInfoComponent,
+    HomeComponent,
+    AboutComponent,
+    NavBarComponent,
+    FooterComponent,
+   
   ],
   imports: [
     BrowserModule,
